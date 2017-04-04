478f3c40211cd3b4994da9972e449ece871e2076
Jacob Schlecht <jacob.lee.schlecht@gmail.com>
2017-04-03 19:23:36 -0500

    Worked on main menu with buttons and stuff

---
+++ Assets/Scripts/button_functions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button_functions : MonoBehaviour {
+
+    [SerializeField]
+    GameObject StartPanel;
+    [SerializeField]
+    GameObject ChoosePanel;
+
+    public void StartButtonFunc()
+    {
+        StartPanel.SetActive(false);
+        ChoosePanel.SetActive(true);
+    }
+
+    public void ExitButtonFunc()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    public void BackButtonFunc()
+    {
+        StartPanel.SetActive(true);
+        ChoosePanel.SetActive(false);
+    }
+
+}
