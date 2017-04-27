4ffc67b9cd5123d8f5a7bdfca03f6babef48f52d
Jacob Schlecht <jacob.lee.schlecht@gmail.com>
2017-04-27 14:41:07 -0500

    Added a mine

---
+++ Assets/Scripts/AI/Enemy_Mine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Mine : MonoBehaviour
+{
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start()
+	{
+		player = GameObject.FindWithTag("Player");
+	}
+
+	void OnCollisionEnter(Collision coll)
+	{
+		if (Data_Manager.IsPlayer(coll.collider))
+		{
+			var PC = coll.gameObject.transform.parent.gameObject.GetComponent(typeof(Player_Controller)) as Player_Controller;
+			if (PC != null)
+			{
+				PC.GameOver();
+				GameObject.Destroy(gameObject);
+			}
+		}
+	}
+}
